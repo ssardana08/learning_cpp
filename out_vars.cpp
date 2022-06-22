f39638dd73132d06fd90251c6d8d032bb1dd92bc
Sumit <sardana.sumit08@gmail.com>
2022-06-22 22:57:21 +0530

    code to print variables

---
+++ out_vars.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+int main() {
+    int num = 1;
+    float price = 256.78;
+    char ch = 'A';
+
+    std::cout << num << std::endl;
+    std::cout << price << std::endl;
+    std::cout << ch << std::endl;
+}
